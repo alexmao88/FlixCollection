5b6aa9e4be78cb515deaec84cea9a98a17dcb993
Alex Mao <alexmao88@gmail.com>
2018-02-12 22:38:58 -0800

    Updated with details and tab bar

--- Flix/ViewControllers/NowPlayingViewController.swift
+++ Flix/ViewControllers/NowPlayingViewController.swift
@@ -97,6 +97,15 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
