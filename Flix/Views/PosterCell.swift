bbef8f8116709e4cccb46e5e3820787dcf583d6c
Alex Mao <alexmao88@gmail.com>
2018-02-13 23:55:49 -0800

    all done

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Alex Mao on 2/12/18.
+//  Copyright © 2018 Alex Mao. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
